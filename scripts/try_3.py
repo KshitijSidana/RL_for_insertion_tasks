86572b3223e4108ffb12d81606a6e9c70675ae13
Kshitij.Sidana <kshitijsidana@gmail.com>
2023-12-02 14:15:14 -0800

    solved linking and qt issues

--- scripts/try_3.py
+++ scripts/try_3.py
@@ -1,75 +1,29 @@
 import os
-
-# import gymnasium as gym
-# import numpy as np 
-from pyrep import PyRep
-from pyrep.robots.arms.ur5 import UR5
 import cv2
+import random
+import numpy as np
+from enum import Enum
 
-# from pyrep.robots.arms.panda import Panda
-# from pyrep.robots.end_effectors.panda_gripper import PandaGripper
-
-
-# pr = PyRep()
-# # Launch the application with a scene file that contains a robot
-# # pr.launch('./sim/broken_w_all_parts.ttt') 
-# pr.launch('./sim/non_pure_non_convex_min_parts.ttt') 
-# # pr.launch('task_design.ttt') 
-# pr.start()  # Start the simulation
-
-
-
-# # 1. Load Environment and Q-table structure
-# # env = gym.make('FrozenLake8x8-v0')
-# env = pr
-# Q = np.zeros([env.observation_space.n,env.action_space.n])
-# # env.observation.n, env.action_space.n gives number of states and action in env loaded
-# # 2. Parameters of Q-learning
-# eta = .628
-# gma = .9
-# epis = 5000
-# rev_list = [] # rewards per episode calculate
-# # 3. Q-learning Algorithm
-# for i in range(epis):
-#     # Reset environment
-#     s = env.reset()
-#     rAll = 0
-#     d = False
-#     j = 0
-#     #The Q-Table learning algorithm
-#     while j < 99:
-#         # env.render()
-#         j+=1
-#         # Choose action from Q table
-#         a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
-#         #Get new state & reward from environment
-#         s1,r,d,_ = env.step(a)
-#         #Update Q-Table with new knowledge
-#         Q[s,a] = Q[s,a] + eta*(r + gma*np.max(Q[s1,:]) - Q[s,a])
-#         rAll += r
-#         s = s1
-#         if d == True:
-#             break
-#     rev_list.append(rAll)
-#     # env.render()
-# print("Reward Sum on all episodes " + str(sum(rev_list)/epis))
-# print("Final Values Q-Table")
-# print(Q)
+from gym import spaces
 
 import gymnasium as gym
-from gymnasium import spaces
+# from gymnasium import spaces
+# from gymnasium.spaces import box
 from gymnasium.utils import seeding
+
+from pyrep import PyRep
+from pyrep.objects.shape import Shape
+from pyrep.robots.arms.ur5 import UR5
 from pyrep.objects.vision_sensor import VisionSensor
 from pyrep.const import TextureMappingMode, RenderMode
-from enum import Enum
-import numpy as np
-import random
-from pyrep.objects.shape import Shape
+
+from stable_baselines3.sac import SAC
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.sac.policies import MlpPolicy
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 
 
 SCENE_FILE = './sim/non_pure_non_convex_min_parts.ttt'
-#join(dirname(abspath(__file__)),
-#     'scene_reinforcement_learning_env.ttt')
 POS_MIN, POS_MAX = [0.8, -0.2, 1.0], [1.0, 0.2, 1.4]
 EPISODES = 5
 EPISODE_LENGTH = 200
@@ -93,17 +47,15 @@ class LqrEnv(gym.Env):
         self.pr.launch(SCENE_FILE, headless)
         self.pr.start()
         
-        # -------------------------------------------------
-        #Fix Err: qt.qpa.plugin: Could not find the Qt platform plugin "xcb" in "/usr/local/lib/python3.8/dist-packages/cv2/qt/plugins" 
-        # os.environ['PATH'] = '$PATH:~/Documents/CSCI_699/CoppeliaSim_Edu_V4_1_0_Ubuntu20_04'
-        # os.environ['COPPELIASIM_ROOT'] = '~/Documents/CSCI_699/CoppeliaSim_Edu_V4_1_0_Ubuntu20_04'
-        # os.environ['LD_LIBRARY_PATH'] = '$LD_LIBRARY_PATH:$COPPELIASIM_ROOT'
-        # os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = '$COPPELIASIM_ROOT'
-        # os.environ['QT_PLUGIN_PATH'] = '~/Documents/CSCI_699/CoppeliaSim_Edu_V4_1_0_Ubuntu20_04/platforms'
-        
         self.episode_length = episode_length
         self.training_type = training_type
-
+        
+        # -------------------------------------------------
+        # Initialize VecNormalize parameters manually
+        self.norm_obs = True
+        self.norm_reward = False
+        self.clip_obs = 255.0  # Adjust clip_obs as needed
+        
         # -------------------------------------------------
         # Creating the Robot and Other objects
         self.agent = UR5()
@@ -113,15 +65,11 @@ class LqrEnv(gym.Env):
         self.agent_ee_tip = self.agent.get_tip()
         self.initial_joint_positions = self.agent.get_joint_positions()
 
-        
-
         # -------------------------------------------------
         # Representation Generation
         # #  TODO !!
         # self.ae_model = LangeConvAutoencoder.load_from_checkpoint(AE_MODEL_PATH)
         # self.ae_model.eval()
-
-
 
         # -------------------------------------------------
         # Setting State space for robot
@@ -137,9 +85,11 @@ class LqrEnv(gym.Env):
                                         high=np.asarray([val[1] for val in self.agent.get_joint_intervals()[1]] + [1. for i in range(28)]), dtype=float)
 
         # -------------------------------------------------
+        # # Normalize Action space between [-1,1]
+        # self.action_space = box.Box(low=-1., high=1., shape=(6,), dtype=float)
         # Normalize Action space between [-1,1]
-        self.action_space = spaces.Box(low=-1., high=1., shape=(6,), dtype=float)
-
+        # self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(6,), dtype=float)
+        self.action_space = self.agent.action_space
         # --------------------------------------------------
         # Sensors
         self.vision_sensor = VisionSensor("kinect_rgb")
@@ -151,43 +101,30 @@ class LqrEnv(gym.Env):
         self.initial_orientation = self.reward_orientation()
         [self.pr.step() for i in range(10)] # Stabilize the simulator
 
-        # self.proximity_sensor = ProximitySensor('ROBOTIQ_85_attachProxSensor')
-        # self.force_sensor = ForceSensor("Force_sensor")
+    def setup_scene(self):
+        # Reset the robot's joint positions
 
-    def reset(self, seed=None, options=None):
-        
-        # # We need the following line to seed self.np_random
-        # super().reset(seed=seed)
+        self.agent.set_joint_positions(self.initial_joint_positions)
+        # self.target.set_position()
 
-        # # Choose the agent's location uniformly at random
-        # self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        # You may need additional code here to reset or initialize other objects in the scene
+        # For example, if there are target positions, you might reset them.
 
-        # # We will sample the target's location randomly until it does not coincide with the agent's location
-        # self._target_location = self._agent_location
-        # while np.array_equal(self._target_location, self._agent_location):
-        #     self._target_location = self.np_random.integers(
-        #         0, self.size, size=len(), dtype=int
-        #     )
+        # Stabilize the simulator
+        [self.pr.step() for i in range(10)]
 
-        # observation = self._get_obs()
-        # info = self._get_info()
-
-        # # if self.render_mode == "human":
-        # #     self._render_frame()
-        # return observation, info
-
-        print("------>",self._get_state())
-        self.setup_scene()
+        # Return the initial state after setup
         return self._get_state()
+    
+    def reset(self, seed=None, options=None):
+        state = self._get_state()
+        print("------>",state)
+        self.setup_scene()
+        if self.norm_obs:
+            state = self.normalize_observation(state)
+        return state
 
     def step(self, action):
-        # costs = np.sum(u**2) + np.sum(self.state**2)
-        # self.state = np.clip(self.state + u, self.observation_space.low, self.observation_space.high)
-        # costs = self.get_costs()
-        
-        # self.pr.step()
-        # return self._get_obs(), -costs, False, {}
-    
         done = False
         info = {}
         prev_distance_to_goal = self.reward_distance_to_goal()
@@ -239,7 +176,26 @@ class LqrEnv(gym.Env):
         self.step_counter += 1
         # print(orientation_reward)
         print(self.step_counter," ",distance_reward*10," ",success_reward," ",orientation_reward," ",reward)
+        # return self._get_state(),reward,done,info
+            
+        
+        if self.norm_obs:
+            next_state = self._get_state()
+            next_state = self.normalize_observation(next_state)
+        else:
+            next_state = self._get_state()
+        
         return self._get_state(),reward,done,info
+    
+    def normalize_observation(self, obs):
+        # Normalize the values to a specific range
+        obs_low = np.asarray([val[0] for val in self.agent.get_joint_intervals()[1]])
+        obs_high = np.asarray([val[1] for val in self.agent.get_joint_intervals()[1]])
+        
+        obs["agent"] = : self._agent_location, "target": self._target_location}
+        
+        normalized_obs = (obs["agent"] - obs_low) / (obs_high - obs_low)
+        return normalized_obs
 
 
     # def _seed(self, seed=None):
@@ -247,11 +203,6 @@ class LqrEnv(gym.Env):
     #     return [seed]
 
     def _get_state(self):
-        # Return state containing arm joint angles/velocities & target position
-        # return np.concatenate([self.agent.get_joint_positions(),
-        #                         self.agent.get_joint_velocities(),
-        #                         self.target.get_position()])
-
         # Return state containing arm joint angles/velocities & target position
         global counter
         counter += 1
@@ -271,20 +222,20 @@ class LqrEnv(gym.Env):
             final_state = np.concatenate((joint_pos , current_representation))
 
 
-        elif self.training_type == TrainingType.VMP_GC:
-            # VMP current image
-            current_image = self.vision_sensor.capture_rgb()
-            current_image = cv2.cvtColor(current_image, cv2.COLOR_BGR2RGB)
-            cv2.imwrite("./"+ str(UUID_val)+"_"+str(counter)+".jpg",current_image*255)
-            current_representation = self.get_representation(current_image).flatten()
+        # elif self.training_type == TrainingType.VMP_GC:
+        #     # VMP current image
+        #     current_image = self.vision_sensor.capture_rgb()
+        #     current_image = cv2.cvtColor(current_image, cv2.COLOR_BGR2RGB)
+        #     cv2.imwrite("./"+ str(UUID_val)+"_"+str(counter)+".jpg",current_image*255)
+        #     current_representation = self.get_representation(current_image).flatten()
 
-            # Goal image
-            goal_image = random.sample(goal_img_list,1)[0]
-            cv2.imwrite("./"+ str(UUID_val)+"_goal_"+str(counter)+".jpg",goal_image)
-            goal_representation = self.get_representation(goal_image/255).flatten()
+        #     # Goal image
+        #     goal_image = random.sample(goal_img_list,1)[0]
+        #     # cv2.imwrite("./"+ str(UUID_val)+"_goal_"+str(counter)+".jpg",goal_image)
+        #     goal_representation = self.get_representation(goal_image/255).flatten()
 
-            joint_pos = self.agent.get_joint_positions()
-            final_state = np.concatenate((joint_pos , current_representation , goal_representation))
+        #     joint_pos = self.agent.get_joint_positions()
+        #     final_state = np.concatenate((joint_pos , current_representation , goal_representation))
 
         # return final_state
 
@@ -357,59 +308,14 @@ class Agent(object):
 
 
 if __name__ == "__main__":
-    from stable_baselines3.sac import SAC
-    from stable_baselines3.sac.policies import MlpPolicy
-    # from stable_baselines3 import A2C
-    
-    from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
-
-    from stable_baselines3.common.monitor import Monitor
-    # from stable_baselines3.common.preprocessing import normalize_obs, normalize_reward
-
     env = LqrEnv()
     # Assuming 'env' is your environment
     env = Monitor(env)  # Wrap with Monitor if needed
     env = DummyVecEnv([lambda: env])  # Wrap with DummyVecEnv
 
     # Use VecNormalize for normalizing observations
-    env = VecNormalize(env, norm_obs=True, norm_reward=False, clip_obs=255.0)
+    # env = VecNormalize(env, norm_obs=True, norm_reward=False, clip_obs=255.0)
 
     model = SAC(MlpPolicy, env, verbose=1)
     model.learn(total_timesteps=25000)
     model.save("a2c_robotic_arm")
-
-    # # import gymnasium as gym
-
-    # from stable_baselines3 import PPO
-    # from stable_baselines3.common.env_util import make_vec_env
-
-    # # Parallel environments
-    # vec_env = make_vec_env(LqrEnv, n_envs=1)
-
-    # model = PPO("MlpPolicy", vec_env, verbose=1)
-    # model.learn(total_timesteps=25000)
-    # model.save("ppo_cartpole")
-
-    # # del model # remove to demonstrate saving and loading
-
-    # model = PPO.load("ppo_cartpole")
-
-    # obs = vec_env.reset()
-    # for i in range (100):
-    #     action, _states = model.predict(obs)
-    #     obs, rewards, dones, info = vec_env.step(action)
-    #     # vec_env.render("human")
-    # vec_env.shutdown()
-
-
-    # # # env = GridworldEnv(5)
-    # # env = LqrEnv()
-    # # states = env.observation_space.shape
-    # # actions = env.action_space.n
-    # # model = build_model(states, actions)
-
-    # # dqn = build_agent(model, actions)
-    # # dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-    # # dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
-
-    # # scores = dqn.test(env, nb_episodes=2, visualize=False)
